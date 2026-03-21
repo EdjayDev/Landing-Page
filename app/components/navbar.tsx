f9ee9b9a9e0f1614d00e07ed257842d73971ebe6
Edjay Jangao <edjay123123@gmail.com>
2026-03-21 22:58:18 +0800

    Updating Navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <div className="w-full flex flex-row p-8 items-center justify-center gap-1 bg-zinc-50 font-sans dark:bg-black">
+      <img src="/logo.png" alt="Logo" className="h-10 w-10" />
       <Link
         href="../"
         className="rounded bg-black px-4 py-2 text-white hover:bg-zinc-800 transition-colors"
